f07d5a106e3f3406053ae263959c81e2c972e768
emotion0809 <a0968683953@gmail.com>
2022-05-20 15:04:00 +0800

    更改port

--- lib/models/api_route_model.dart
+++ lib/models/api_route_model.dart
@@ -11,43 +11,43 @@ class Situation {
 
 List<Situation> Situations = [
   Situation(
-    name: '點酒',
+    name: '選餐廳',
     port: '5001',
   ),
   Situation(
-    name: '點甜點',
-    port: '5003',
+    name: '餐廳訂位',
+    port: '5004',
   ),
   Situation(
     name: '餐廳帶位',
-    port: '5005',
-  ),
-  Situation(
-    name: '選擇餐廳',
     port: '5007',
   ),
   Situation(
-    name: '預約',
-    port: '5009',
-  ),
-  Situation(
-    name: '點主菜',
-    port: '5011',
-  ),
-  Situation(
-    name: '點食物與飲料',
-    port: '5013',
+    name: '點食物和飲料',
+    port: '5010',
   ),
   Situation(
     name: '點開胃菜',
-    port: '5015',
+    port: '5013',
+  ),
+  Situation(
+    name: '點主菜',
+    port: '5016',
+  ),
+  Situation(
+    name: '點酒',
+    port: '5019',
+  ),
+  Situation(
+    name: '點點心',
+    port: '5022',
   ),
   Situation(
     name: '給好評',
-    port: '5017',
+    port: '5025',
   ),
   Situation(
     name: '給負評',
-    port: '5019',
+    port: '5028',
   ),
 ];
